18ad36021c8e1ec5cec697c7a568176c96d799b4
Haeward <code@haeward.com>
2025-03-20 20:20:21 +0800

    Migrate about & now path

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,46 +7,9 @@ const blog = defineCollection({
     description: z.string(),
     date: z.coerce.date(),
     draft: z.boolean().optional(),
-    categories: z.array(z.string()).optional(),
+    categories: z.string().optional(),
     tags: z.array(z.string()).optional(),
   }),
 });
 
-const work = defineCollection({
-  type: "content",
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.coerce.date(),
-    dateEnd: z.union([z.coerce.date(), z.string()]),
-  }),
-});
-
-const projects = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-    repoURL: z.string().optional(),
-    demoURL: z.string().optional(),
-  }),
-});
-
-const about = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-  }),
-});
-
-const now = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    updated : z.coerce.date(),
-  }),
-});
-
-export const collections = { blog, work, projects, about, now };
+export const collections = { blog };
---
+++ src/content/now.md
@@ -0,0 +1,33 @@
+---
+title: "Now"
+updated: "2025-03-19"
+---
+
+##### Current Focus
+
+- Start running
+- Learning Japanse
+- Learning data structures and algorithms
+
+##### Reading
+
+- [平面国：多维空间传奇往事](https://www.goodreads.com/book/show/55661114)
+- [失明症漫记](https://www.goodreads.com/book/show/29865870)
+- [献给阿尔吉侬的花束](https://www.goodreads.com/book/show/28477658)
+- [葬送のフリーレン](https://bangumi.tv/subject/305429)
+
+##### Watching
+
+- [俺だけレベルアップな件 Season 2 -Arise from the Shadow](https://bangumi.tv/subject/487630)
+- [全修。](https://bangumi.tv/subject/486039)
+- [凡人修仙传：外海风云](https://bangumi.tv/subject/518425)
+- [SAKAMOTO DAYS](https://bangumi.tv/subject/496617)
+- [ダンダダン](https://bangumi.tv/subject/467461)
+- [Severance Season 2](https://neodb.social/tv/season/2iGcyQRlEuz7fvMuAIlsKK)
+- [Mickey 17](https://neodb.social/movie/1THIfas0TCJHselxC8eFpO)
+- [The Brutalist](https://neodb.social/movie/7Ewenx9aHugGnoOV2DAbs0)
+
+##### Gaming
+
+- [Balatro](https://store.steampowered.com/app/2379780/Balatro/)
+- [Slay the Spire](https://store.steampowered.com/app/646570/Slay_the_Spire/)
