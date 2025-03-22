caded15ba2e6c4bd8543e4e36e14fecc08b7d7ed
Haeward <haewardy@gmail.com>
2025-03-22 18:48:36 +0800

    Add statistics heatmap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
 export default defineConfig({
   site: "https://blog.haeward.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
