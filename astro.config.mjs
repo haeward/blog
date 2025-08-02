dd7c79de300644b4a746aef6a069eac13095a99f
Haeward <code@haeward.com>
2025-08-02 22:57:56 +0800

    feat: update site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import embeds from "astro-embed/integration";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: "https://blog.haeward.com",
+  site: "https://haeward.com",
 
   integrations: [sitemap(), tailwind(), react(),
     embeds({
