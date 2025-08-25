dde03a152312938356fb6732fd50b0a49ad01f7d
Haeward <code@haeward.com>
2025-08-25 14:12:21 +0800

    fix: fix RSS image display

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,32 +1,43 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE } from "@consts";
-import sanitizeHtml from "sanitize-html";
-import MarkdownIt from "markdown-it";
-
-const parser = new MarkdownIt();
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
+import { loadRenderers } from "astro:container";
 
 type Context = {
   site: string
 }
 
 export async function GET(context: Context) {
+  const renderers = await loadRenderers([getMDXRenderer()]);
+  const container = await AstroContainer.create({ renderers });
+
   const blog = (await getCollection("blog")).filter(post => !post.data.draft);
 
-  const items = [...blog]
+  const posts = [...blog]
     .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+
+  const items = await Promise.all(
+    posts.map(async (post) => {
+      const { Content } = await post.render();
+      const html = await container.renderToString(Content);
+
+      return {
+        title: post.data.title,
+        description: post.data.description,
+        content: html,
+        pubDate: post.data.date,
+        link: `/${post.collection}/${post.slug}/`,
+      };
+    })
+  );
 
   return rss({
     title: SITE.NAME,
     description: SITE.DESC,
     site: context.site,
-    items: items.map((item) => ({
-      title: item.data.title,
-      description: item.data.description,
-      content: sanitizeHtml(parser.render(item.body)),
-      pubDate: item.data.date,
-      link: `/${item.collection}/${item.slug}/`,
-    })),
+    items: items,
 
     customData: `
       <follow_challenge>
